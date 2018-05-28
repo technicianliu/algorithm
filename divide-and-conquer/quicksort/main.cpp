84d0748fbe90fdf26f3af76d8489256b650f4a8d
algorithm <techliu@dingtalk.com>
2018-05-28 15:55:16 +0800

    Update main.cpp

--- divide-and-conquer/quicksort/main.cpp
+++ divide-and-conquer/quicksort/main.cpp
@@ -26,22 +26,22 @@ void Swap(int &a,int &b)
 
 void quicksort(int a[],int s,int e)
 {
-    if(s>=e) //�ݹ��������
+    if(s>=e) //递归结束条件
         return;
 
     int k=a[s],i=s,j=e;
 
-    while(i!=j) //���ﲻ������Ϊ�ݹ��������
+    while(i!=j) //这里不可以作为递归结束条件
     {
-        while(j>i&&a[j]>k) //����ż���ν����󣬾Ͳ�ͣ����a[j]��a[i]���бȽϡ�
+        while(j>i&&a[j]>=k) //发生偶数次交换后，就不停的拿a[j]与a[i]进行比较。
             j=j-1;
-        Swap(a[i],a[j]);   //ͨ����������ͰѴӺ���ǰ��һ�������ı�kС���Ǹ���������kǰ�档
+        Swap(a[i],a[j]);   //通过这个交换就把从后往前第一次遇到的比k小的那个数换到了k前面。
 
-        while(i<j&&a[i]<k) //���������ν����󣬾Ͳ�ͣ����a[i]��a[j]���бȽ�
+        while(i<j&&a[i]<k) //发生奇数次交换后，就不停的拿a[i]与a[j]进行比较
             i=i+1;
-        Swap(a[i],a[j]);   //��kǰ�������ĵ�һ����k�����������k���档
+        Swap(a[i],a[j]);   //把k前面遇到的第一个比k大的数换到了k后面。
     }
-    //����ִ����󣬵�i����kǰ�涼�Ǳ�kС�ģ����涼�Ǵ��ڻ����k��
+    //上面执行完后，第i个数k前面都是比k小的，后面都是大于或等于k的
     quicksort(a,s,i-1);
     quicksort(a,i+1,e);
 }
